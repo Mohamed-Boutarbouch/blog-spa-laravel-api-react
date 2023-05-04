929c03b04a891c7af0870404f2342789df053421
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2023-05-04 17:41:55 +0100

    models relationships defined

--- api/app/Models/Article.php
+++ api/app/Models/Article.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'excerpt', 'description', 'status', 'user_id', 'category_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
